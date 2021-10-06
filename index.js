e668e135583358d2faaaf39349cc224a313c187c
Faustina Lestrade <faustiles99@gmail.com>
2021-10-05 21:45:35 -0300

    Navigation and folder setup

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+import ScreenStack from "./src/features/navegation/views/ScreenStack";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ScreenStack);
---
+++ src/features/artistDetails/views/ArtistDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ArtistDetails = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Artist details screen</Text>
+    </View>
+  );
+};
+
+export default ArtistDetails;
